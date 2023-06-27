83145c9bfcffeba6eb8ad884c22b67c4af41e890
daniel <danielgr21120@gmail.com>
2023-06-27 17:59:09 +0200

    extended the weather dto

--- src/main/java/com/daniel/simpleweather/dto/WeatherDTO.java
+++ src/main/java/com/daniel/simpleweather/dto/WeatherDTO.java
@@ -13,29 +13,47 @@ import java.util.Map;
 @Data
 public class WeatherDTO {
 
-
+    private String main;
     private String description;
     private BigDecimal temp;
+    private BigDecimal temp_min;
+    private BigDecimal temp_max;
+    private String pressure;
+    private String humidity;
+    private String wind_speed;
     private String country;
     @JsonProperty("name")
     private String city;
 
     @JsonProperty("weather")
-    private void unpackDescriptionFromNestedObject(Map<String, String>[] weather){
+    private void unpackWeatherDetailsNestedObject(Map<String, String>[] weather) {
+        this.main = weather[0].get("main");
         this.description = weather[0].get("description");
     }
 
+//    @JsonProperty("weather")
+//    private void unpackDescriptionFromNestedObject(Map<String, String>[] weather) {
+//        this.description = weather[0].get("description");
+//    }
+
     @JsonProperty("main")
-    private void unpackTempFromNestedObject(Map<String, String> main){
+    private void unpackTempDetailsFromNestedObject(Map<String, String> main) {
         this.temp = BigDecimal.valueOf(Double.parseDouble(main.get("temp")));
+        this.temp_min = BigDecimal.valueOf(Double.parseDouble(main.get("temp_min")));
+        this.temp_max = BigDecimal.valueOf(Double.parseDouble(main.get("temp_max")));
+        this.pressure = main.get("pressure");
+        this.humidity = main.get("humidity");
     }
+
+
+    @JsonProperty("wind")
+    private void unpackWindSpeedFromNestedObject(Map<String, String> wind) {
+        this.wind_speed = wind.get("speed");
+    }
+
 
     @JsonProperty("sys")
-    private void unpackCountryFromNestedObject(Map<String, String> sys){
+    private void unpackCountryFromNestedObject(Map<String, String> sys) {
         this.country = sys.get("country");
     }
-
-
-
-
 }
